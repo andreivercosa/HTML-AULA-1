3b0166240394809e688170758c05679cf6e3773c
andreivercosa <andreivercosa@gmail.com>
2019-01-03 21:37:59 -0400

    Aula 01

--- src/mainAnimal.js
+++ src/mainAnimal.js
@@ -3,6 +3,22 @@ function dados(){
     var raca = document.getElementById("racaAnimal").value;
     var idade = document.getElementById("idadeAnimal").value;
     //alert("Ola, seja bem vindo " + nome + " " + sobrenome + " Idade: " + idade);
+    
+    
+    if(!nome){
+        alert("Informe o Nome")
+        return false
+    }
+
+    if(!raca){
+        alert("Informe a Raça")
+        return false
+    }
+
+    if(!idade){
+        alert("Informe a Idade")
+        return false
+    }
     var d = "Nome: " + nome + " Raça: " + raca + " Idade: " + idade;
     document.getElementById("comit").value = d;
 }
